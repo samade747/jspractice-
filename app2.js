1f8ca07baf0437d5a47441797d02fb4d9fd54502
samade747 <samad.e747@gmail.com>
2023-08-17 01:11:22 +0500

    3rd one chapter

---
+++ app2.js
@@ -0,0 +1,34 @@
+// chapter 03
+
+// 01
+var Age
+Age = "30years";
+alert(Age)
+
+
+// 02
+var visit
+var times
+times = "14 times"
+visit = " You have visited this site"
+alert(visit +  times);
+
+//03
+var birthYear; 
+birthYear = "My Birth year is 1990";
+alert(birthYear);
+
+
+//04
+var VisitorName
+var ProductTitle
+var Quantity
+var StoreName 
+VisitorName = "John Doe";
+ProductTitle = 'T-shirt(s)';
+Quantity = "ordered 5";
+StoreName = "XYZ Clothing Store";
+document.write(VisitorName + Quantity + ProductTitle + StoreName)
+
+
+
