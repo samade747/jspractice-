d038159c808f4fa88515c38bb073909ac0cf6b66
samade747 <samad.e747@gmail.com>
2023-08-21 22:08:58 +0500

    with double prompt

--- appforloop.js
+++ appforloop.js
@@ -22,3 +22,22 @@ var userinp = +prompt("Enter Value")
 for(var i=1;  i<=10;               i++            ){
     document.write(userinp + " x " +i+ "=" +userinp*i, "<br>")
 }
+
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+
+
+// with user input with 2 prompt 
+
+var userinp = +prompt("Enter Value")
+var userinp1 = +prompt("Enter Value")
+
+
+// with user input // intiazise  / condition check / increment decrement 
+for(var i=1;  i<=userinp1;               i++            ){
+    document.write(userinp + " x " +i+ "=" +userinp*i, "<br>")
+}
