32d006b55db860d1167af3dc7bfe95532c0ec9d0
samade747 <samad.e747@gmail.com>
2023-08-21 21:47:46 +0500

    for loop

---
+++ appforloop.js
@@ -0,0 +1,24 @@
+
+// intiazise  / condition check / increment decrement 
+for(var i=1;  i<=10;               i++            ){
+    document.write(2 + " x " +i+ "=" +2*i, "<br>")
+}
+
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+
+
+
+// with user input 
+
+var userinp = +prompt("Enter Value")
+
+
+// with user input // intiazise  / condition check / increment decrement 
+for(var i=1;  i<=10;               i++            ){
+    document.write(userinp + " x " +i+ "=" +userinp*i, "<br>")
+}
