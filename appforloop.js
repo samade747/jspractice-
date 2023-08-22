184376d3b314a369497ef70d402fe80a2fe4ec6b
samade747 <samad.e747@gmail.com>
2023-08-22 23:58:26 +0500

    2nested loop2

--- appforloop.js
+++ appforloop.js
@@ -80,8 +80,8 @@ for(var i=0; i<arr1.length; i++){
 
 for(var i=1; i<=100; i=i+10){
 
-    for(var j=i; j < i+10; j++)
-    document.write(j+ " ")  
+    for(var j=i; j < i+10; j++){
+    document.write(j+" ")  
 }
     document.write("<br>")
-
+}
