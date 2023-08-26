7ce7482fd6fa6535b2235ece69f0403958227821
samade747 <samad.e747@gmail.com>
2023-08-26 19:37:45 +0500

    calcualtor

--- appforloop.js
+++ appforloop.js
@@ -279,3 +279,10 @@ function eq(){
     var a = document.getElementById('inp')
     a.value =eval(a.value)
 }
+
+function calculatePercentage() {
+    var inputField = document.getElementById('inp');
+    var expression = inputField.value;
+    var result = eval(expression) / 100;
+    inputField.value = result;
+}
