e309286900d22219dbdedbf8a21d0f8b3336bdd0
samade747 <samad.e747@gmail.com>
2023-08-30 23:51:56 +0500

    do to app

--- appforloop.js
+++ appforloop.js
@@ -379,4 +379,76 @@ function reset(){
 
  
 
+
+// crued
+// create read update edit delete 
+
 // creating To Do App 
+
+// // getAttribute
+// function foo(){ 
+//     var a = document.getElementById('inp')
+//     var b = a.getAttribute('name')
+//     console.log(b)
+// }
+
+// // has attribute
+// function foo(){ 
+//     var a = document.getElementById('inp')
+//     var b = a.hasAttribute('class')
+//     console.log(b)
+// }
+
+
+// // set attribute // agar nai hai toh lga do 
+// function foo(){ 
+//     var a = document.getElementById('inp1')
+//     var b = a.setAttribute('class','aaa')
+//     console.log(b)
+// } 
+
+
+// creating To Do App 
+
+
+
+var getul = document.getElementById('ul')
+
+function foo1(){
+    var ca = document.getElementById('inp2')
+    var li = document.createElement('li')
+    var litext= document.createTextNode(ca.value)
+    li.appendChild(litext)
+    getul.appendChild(li)
+    ca.value = ''
+    var deletebtn = document.createElement('button')
+    var deletebtnText = document.createTextNode("Delete")
+    del
+    li.appendChild(deletebtn)
+}
+
+function deleteall(){
+    getul.innerHTML = ''
+}
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
