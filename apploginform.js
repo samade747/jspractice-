02e491b863a28602113ab48a19f1c26e1e0b973c
samade747 <samad.e747@gmail.com>
2023-09-02 02:58:37 +0500

    12

---
+++ apploginform.js
@@ -0,0 +1,30 @@
+
+
+// signup Values
+
+var firstName = document.getElementById('firstName')
+var lastName = document.getElementById('lastName')
+var userName = document.getElementById('userName')
+var email = document.getElementById('email')
+var password = document.getElementById('password')
+var cPassword = document.getElementById('cPassword')
+
+
+// singup fucntion
+
+function singuphandler(){
+    
+// checking if empty value
+
+    if(firstName.value == '' ||
+    
+    
+    
+    
+    
+    
+    
+    )
+
+
+}
