56d80513c3fc27a1ad29600926c4bc9a0e35b99e
samade747 <samad.e747@gmail.com>
2023-09-03 14:24:20 +0500

    login form

--- apploginform.js
+++ apploginform.js
@@ -1,8 +1,8 @@
 
 
 
-var userEmail 
-var userPassword
+var userEmail = "samad.e747@gmail.com";
+var userPassword = "12345678";
 
 
 
@@ -13,12 +13,12 @@ var lastName = document.getElementById('lastName')
 var userName = document.getElementById('userName')
 var email = document.getElementById('email')
 var password = document.getElementById('password')
-var cPassword = document.getElementById('cPassword')
+var cpassword = document.getElementById('cpassword')
 
 
 // singup fucntion
 
-function singuphandler(){
+function singuphandler() {
     
 // checking if empty value
 
@@ -26,24 +26,43 @@ function singuphandler(){
        lastName.value  == '' ||
        userName.value  == '' ||
        password.value  == '' ||
-       cPassword.value == '' ||
-       email.value     == ''  ) {
-        return alert('fillup all fields')        
+       cpassword.value == '' ||
+       email.value     == ''
+       ) {
+        alert('Please fill in all fields');
+        return;
        }    
     // password Matching 
-    if (password.value != cPassword.value) {
-        return alert('password and confirm mismatched')
+    if (password.value !== cpassword.value) {
+        alert('Password and confirm password do not match.');
+        return;
     }
 
-    // checking if already user exist 
+    // Email validation using a regular expression
+    var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    if (!emailPattern.test(email.value)) {
+       alert('Invalid email address.');
+       return;
+    }
 
-    if (email.value == userEmail){
-        return alert('account already exist')
-    } 
+    // passowrd length minimum
+    if (password.value.length < 8) {
+        alert('password minimum 8 characters long');
+        return;
+    }
 
+    // Assuming you want to redirect only if signup is successful
+
+    alert('user sign up succesfullly');
+    window.location.href = './login.html';  
+}
+
+
+// loginvalues 
+const loginEmail = document.getElementById('loginemail')
+const loginPassword = document.getElementById('loginpassword')
+
+
+function loginhandler(){
     
-    
-    
-
-
 }
