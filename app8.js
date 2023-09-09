fc0c6d1eb92b605e4f3d6b85d0f65e7d9f90c94e
samade747 <samad.e747@gmail.com>
2023-09-09 23:53:13 +0500

    1

---
+++ app8.js
@@ -0,0 +1,46 @@
+// chapter 6-9
+
+// 01 
+// prgm 1
+let prgm1 = prompt(10);
+
+document.write('The Value of a is :' +prgm1)
+
+document.write('<br>');
+document.write('<br>');
+// prgm2
+let prgm2 = ++prgm1;
+document.write('The Value of ++a is' + prgm1);
+document.write('<br>');
+document.write('Now the Value of a is:' +prgm2);
+
+document.write('<br>');
+// prgm3
+let prgm3 = prgm2++;
+document.write('<br>');
+document.write('The Value of a++ is' + prgm3);
+
+document.write('<br>');
+document.write('Now the Value of a is:' + ++prgm3);
+
+// prgm4
+document.write('<br>');
+let prgm4 = --prgm3;
+document.write('<br>');
+document.write('The Value of --a is' + prgm4);
+
+document.write('<br>');
+document.write('now the Value of a is:' +prgm4);
+
+// prgm5
+document.write('<br>');
+let prgm5 = prgm4--;
+document.write('<br>');
+document.write('The Value of a-- is' + prgm5);
+
+document.write('<br>');
+document.write('The Value of a is:' + --prgm5);
+
+
+// 02 
+
