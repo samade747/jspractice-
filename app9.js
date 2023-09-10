8abf3006ecdccb3f0a88614d030e0a7751994914
samade747 <samad.e747@gmail.com>
2023-09-10 16:09:17 +0500

    chapter 9 -11

--- app9.js
+++ app9.js
@@ -24,16 +24,55 @@
 
 // 003 
 
-let color = prompt('Enter color of Traffic Signal (Red, Yellow, Green)');
+// let color = prompt('Enter color of Traffic Signal (Red, Yellow, Green)');
 
-if (color.toLowerCase() === 'red') {
-    document.write('Must Stop');
-} else if (color.toLowerCase() === 'yellow') {
-    document.write('Get Ready to Move');
-} else if (color.toLowerCase() === 'green' ) {
-    document.write('GO Now');
-} else { 
-    document.write('Type Only Red, Green, or Yellow. Wrong input.');
+// if (color.toLowerCase() === 'red') {
+//     document.write('Must Stop');
+// } else if (color.toLowerCase() === 'yellow') {
+//     document.write('Get Ready to Move');
+// } else if (color.toLowerCase() === 'green' ) {
+//     document.write('GO Now');
+// } else { 
+//     document.write('Type Only Red, Green, or Yellow. Wrong input.');
+// }
+
+// document.write('<br>');
+
+// // 004
+
+// const fuel = +prompt('enter remining fuel in car(in liters): ');
+// if ( fuel <= 0.25 ) {
+//     document.write('pls Refill the fuel or ready to dka lgaing');
+// }
+
+// document.write('<br>');
+
+// 05 
+
+let a1 = 4;
+document.write('fixed value '+ a1);
+document.write('<br>');
+
+if (++a1 === +prompt('your Number')) {
+    document.write('Is True ');
+} else {
+    document.write('Is False');
 }
 
-document.write('<br>');
+
+// 05 part b
+
+var b = 82;
+if (b++ === 83){
+    document.write("b is true");
+}
+
+// 5 part c 
+
+var c = 12;
+if (c++ === 13){
+    document.write("b is true");
+} if 
+
+
+
