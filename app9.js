70dc748dd0465a863ac402d7ce74452dfd3f78c2
samade747 <samad.e747@gmail.com>
2023-09-11 23:29:50 +0500

    11

--- app9.js
+++ app9.js
@@ -72,7 +72,121 @@ if (b++ === 83){
 var c = 12;
 if (c++ === 13){
     document.write("b is true");
-} if 
+} 
+
+if (c === 13){
+    document.write("condition 2 is true");
+}
+
+if (++c < 14){
+    document.write("condition 3 is true");
+}
+
+if (c === 14){
+    document.write("condition 14 is true");
+}
+
+// 5 part D 
+
+var MaterialCost = 20000;
+var laborCost = 2000;
+var totalCost = MaterialCost + laborCost;
+if (totalCost === laborCost + MaterialCost){
+    document.write('the cost equals');
+}
+if (true) {
+    document.write('true');
+}
+if (false) {
+    document.write('false');
+}
+
+// 5 f
+if ('car' < 'cat'){
+    document.write('car is smaller than cat')
+}
+
+// 006
+
+document.write('<h2>Marks Sheets</h2>');
+let totalMarks = +prompt('Total Marks:');
+document.write('<h3>Total Marks : </h3>' + totalMarks);
+
+let obtainedMarks = +prompt('Enter marks obtained in three subjects: ');
+document.write('<h3>Marks Obtained: </h3>' + obtainedMarks);
+
+let percentage = (obtainedMarks/totalMarks) * 100;
+document.write('<h3>Percentage: </h3>' + percentage + '<h3>%</h3>');
+if (percentage >= 80){
+    document.write('<h3>Grade: A-One</h3>');
+    document.write('<h3>Remarks: Excellent</h3>');
+} else if (percentage >= 70){
+    document.write('<h3>Grade: A</h3>');
+    document.write('<h3>Remarks: Good</h3>');
+} else if (percentage >= 60 ) {
+    document.write('<h3>Grade: B</h3>');
+    document.write('<h3>Remarks: You need to improve</h3>');
+} else if (percentage < 60 ) {
+    document.write('<h3>Grade: Fail</h3>');
+    document.write('<h3>Remarks: Sorry</h3>');
+}
+
+document.write('<br>');
+
+//007
+document.write('<h2>Guess Game</h2>');
+const secretNumber = 5;
+const guessNumber = +prompt('Enter the number inrange 1 to 10');
+if ( secretNumber === guessNumber) {
+    document.write('<h3>Bingo! Correct answer</h3>')
+} else if (++secretNumber === guessNumber){
+    document.write('<h3>Close enough to correct answer</h3>')       
+} else {
+    document.write('<h3>Wrong Answer</h3>');
+}
 
 
+document.write('<br>');
+
+// 008
+
+const givenNumber = +prompt('enter the number: ');
+if (givenNumber % 3 === 0) {
+    document.write('the number is divisble by 3');
+} else {
+    document.write('the number is not divisible by 3')
+}
+
+document.write('<br>');
+
+
+// 009 checking give number is odd or even
+
+let inputNumber1 = +prompt('enter the number: ');
+if ( inputNumber1 % 2 === 0) {
+    document.write('<h3>Even Number</h3>');
+} else {
+    document.write('<h3>Odd Number</h3>');
+}
+
+
+document.write('<br>');
+
+// 010 
+
+let temperature = +prompt('Enter the temperature: ');
+if (temperature > 40) {
+    document.write('it is too hot outside');
+} else if (temperature > 30) {
+    document.write('Normal weather');    
+} else if (temperature > 20) {
+    document.write('Cool Weather');    
+} else if (temperature > 00) {
+    document.write('wow ! Today Weather is cool ');    
+}
+
+document.write('<br>');
+
+
+// 011
 
