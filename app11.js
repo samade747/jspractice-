3a745ab77cd3bf557fb1396d7e94c4a7c9013518
samade747 <samad.e747@gmail.com>
2023-09-12 22:23:39 +0500

    exc. new

--- app11.js
+++ app11.js
@@ -12,4 +12,42 @@ if((charater1 >= 65)&&(charater1 <= 90)){
 
 document.write('<br>'); 
 
-// 002
+// 002 "OR operator any single condition is true"
+
+const intergar1 = +prompt('Enter the first integer: ');
+const intergar2 = +prompt('Enter the first integer: ');
+if((intergar1 > intergar2)||(intergar2 < intergar1)) {
+    document.write(intergar1 + 'is larger than' + intergar2);
+
+}else if ((intergar2 > intergar1) || (intergar1 < intergar2)){
+    document.write(intergar2 + 'is larger than' + intergar1);
+} else if (intergar1 + 'is equal to' + intergar2);{
+    document.write(intergar1 + 'is equal to' + intergar2);
+}
+
+
+document.write('<br>'); 
+
+
+// 003
+
+const userNumber = +prompt('Enter the Number: ');
+if (userNumber < 0){
+    document.write('<h3>The number is negative</h3>')
+} else if(userNumber === 0) {
+    document.write('<h3>The number is zero</h3>')
+} else {
+    document.write('<h3>The number is positive</h3>')
+}
+
+document.write('<br>'); 
+
+// 004
+
+
+
+
+
+
+
+
