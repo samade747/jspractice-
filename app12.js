198c31c2dedf777eb832d76b91d4c911e3d5bab3
samade747 <samad.e747@gmail.com>
2023-09-13 18:04:59 +0500

    12

--- app12.js
+++ app12.js
@@ -1,8 +1,58 @@
-// chapter 12 13
+// chapter 14 16
 
 // 01
 let student = [];
 // 02
 let students = {};
 // 03 declare & intialize a string array
-let 
+let str_Array = ['a', 'b', 'c', 'd'];
+// 04 
+const num_Array = [1,2,3,4,5];
+//05
+let boolean_Array = [true, false];
+// 06
+let mixed_Array = ['abc', 'def', 20, 30, true];
+//07
+const qualifications = ['SSC', 'HSC', 'BCS', 'BS', 'BCOM', 'MS', 'M.Phil', 'PHD'];
+document.write("1) ", qualifications[0], '<br>')
+document.write("2) ", qualifications[1], '<br>')
+document.write("3) ", qualifications[2], '<br>')
+document.write("4) ", qualifications[3], '<br>')
+document.write("5) ", qualifications[4], '<br>')
+document.write("6) ", qualifications[5], '<br>')
+document.write("7) ", qualifications[6], '<br>')
+document.write("8) ", qualifications[7], '<br>')
+
+document.write('<hr>');
+
+// 08
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
