b6cf164689c1ff8f42b163efbcdf6c578d1986ab
samade747 <samad.e747@gmail.com>
2023-09-13 18:25:23 +0500

    16

--- app12.js
+++ app12.js
@@ -27,13 +27,40 @@ document.write('<hr>');
 
 // 08
 
+var student_Names = ['haroon', 'kamil', 'Samir bawle', 'samad'];
+var student_Score = [320,230,180, 499];
+var total_Marks = 500;
+var percentage1 = (student_Score[0]/total_Marks) * 100;
+document.write('Score of' + student_Names[0] + 'is' + student_Score[0] + '.' + 'Percentage:' + percentage1 + '%');
+document.write('<hr>');
+var percentage2 = (student_Score[1]/total_Marks) * 100;
+document.write('Score of' + student_Names[1] + 'is' + student_Score[1] + '.' + 'Percentage:' + percentage2 + '%');
+document.write('<hr>');
+
+var percentage3 = (student_Score[2]/total_Marks) * 100;
+document.write('Score of' + student_Names[2] + 'is' + student_Score[2] + '.' + 'Percentage:' + percentage3 + '%');
+document.write('<hr>');
+
+// 09
 
 
-
-
-
-
-
+let colors = ['black', 'blue', 'white',];
+document.write(colors + '<br>');
+//09 A
+colors.unshift(prompt('which color you want to add at the begining of array?'));
+document.write(colors + '<br>');
+// 09 B 
+colors[colors.length] = prompt('end add array ?');
+document.write(colors + '<br>');
+//9 c
+colors.unshift(prompt('Begining Add'),prompt('add again begining'));
+document.write(colors + '<br>');
+// 9 d 
+colors.shift();
+document.write(colors + '<br>');
+// 9 e
+colors.pop();
+document.write(colors + '<br>');
 
 
 
