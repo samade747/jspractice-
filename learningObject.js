ca906de2cac91d90315c569560581907b2a14360
samade747 <samad.e747@gmail.com>
2023-09-19 23:27:32 +0500

    ln obj

---
+++ learningObject.js
@@ -0,0 +1,19 @@
+
+
+// chapter Object 
+
+// // for in loop for object
+
+// var obj = {
+//     s_id : '01',
+//     s_name : 'samad ',
+//     s_age : 33,
+//     s_roll : 'CSC-20s=088',
+//     s_depart : 'cs'
+// }
+
+
+
+// for(var key in obj){
+//     document.write(key, ' : ', obj[key], "<br>");
+// } 
