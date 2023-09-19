97e9bd478b1dd1190637600898191f6de0b65e4d
samade747 <samad.e747@gmail.com>
2023-09-20 01:32:43 +0500

    kjmn

--- learningObject.js
+++ learningObject.js
@@ -17,3 +17,36 @@
 // for(var key in obj){
 //     document.write(key, ' : ', obj[key], "<br>");
 // } 
+
+function signup(){
+    var email = document.getElementById('semail').value
+    var pass = document.getElementById('spass').value
+    localStorage.setItem('Email',email)
+    localStorage.setItem('password',pass)
+}
+
+function signin(){
+    var email = document.getElementById('lemail').value
+    var pass = document.getElementById('lpass').value
+    if(localStorage.getItem('Email') == email && localStorage.getItem('password') == pass){
+        location.href = './welcome.html'
+    }
+    else{
+    alert('invalid user name password');
+    }
+}
+
+
+ 
+
+// // date
+ 
+// var date = new Date();
+// var convert = date.toString();
+// var copy = convert.slice(16,24);
+// document.write(copy +  '<br>');
+
+// // 
+// var date = new Date ()
+// var getday = date.getDay()
+// document.write(getday + '<br>')
