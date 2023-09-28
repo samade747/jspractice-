75ce7e5919baf67766d00bb157251bf8fcac49b9
samade747 <samad.e747@gmail.com>
2023-09-28 13:24:23 +0500

    212122

---
+++ Arraymethods.js
@@ -0,0 +1,29 @@
+// indexof learning Array indexof method 
+
+// var myFreindNames = ['samad', 'noman', 'abdulrauf', 'ahmed' ]
+
+
+// finding index with name 'noman' and provide the search start index in numbers
+
+// console.log(myFreindNames.indexOf('noman', 0));
+
+
+// lasIndexOf
+// var myFreindNames = ['samad', 'noman', 'abdulrauf', 'ahmed' ]
+
+// // start from last 
+
+// console.log(myFreindNames.lastIndexOf('noman',3));
+
+
+
+//includes 
+
+var myFriendNames = ['samad', 'noman', 'abdulrauf', 'ahmed' ]
+
+//if value is inculde its reply true & false
+// determines whether the array contains a value
+// returning true or false 
+// no backward search, only forward search
+console.log(myFriendNames.includes('noman'));
+console.log(myFriendNames.includes('ahmed'));
