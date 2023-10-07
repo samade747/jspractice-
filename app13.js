2dc9e8bf4a8d10755c3960f6d22277397e466f67
samade747 <samad.e747@gmail.com>
2023-10-07 22:16:32 +0500

    aasdads

--- app13.js
+++ app13.js
@@ -51,14 +51,90 @@
 // e. Series: 2k, 4k, 6k, 8k, 10k, 12k, 14k, 16k, 18k, 20k
 
 
-
+//A
 // for(var i=1; i<=15; i++){
 //   document.write(i + '<br>');
 // }
 
 
+// //b
+// for(var j=10; j>=1; j--){
+//     document.write(j + '<br>');
+//   }
 
-for(var j=10; j>=1; j--){
-    document.write(j + '<br>');
-  }
+// //c
+// for( let i = 0; i <= 20; i +=2 ){
+//   document.write(i+ '<br>');
+// }
 
+
+// //d
+// for( let i = 1; i <= 19; i +=2 ){
+//   document.write(i+ '<br>');
+// }
+
+
+// //e
+// for( let i = 2; i <= 20; i +=2 ){
+//   document.write(i+ 'k'+ '<br>');
+// }
+
+
+// 7. You have an array
+// A = [“cake”, “apple pie”, “cookie”, “chips”, “patties”]
+// Write a program to enable “search by user input” in an 
+// array.
+// After searching, prompt the user whether the given item is
+// found in the list or not. Example
+
+
+// const bakery = [
+//   'cake',
+//   'apple pie',
+//   'cookie',
+//   'chips',
+//   'patties'
+// ]
+
+
+// function searchItem(){
+//   const userInput = prompt('Enter an item to search: ');
+
+//   if(bakery.includes(userInput)){
+//     document.write(`${userInput} is found in the list`);
+//   } else {
+//     document.write(`${userInput} is not found in the list`);
+//   }
+// }
+
+// searchItem();
+
+
+
+// 8. Write a program to identify the largest number in the 
+// given array.
+// A = [24, 53, 78, 91, 12]
+
+// const A = [24, 53, 78, 91, 500, 500.5, 12];
+
+// function findLargestNumber(arr) {
+//   let max = arr[0];
+
+//   for(let i = 1; i < arr.length; i++){
+//     if(arr[i] > max ){
+//       max = arr[i];
+//     }
+//   }
+//   return max; 
+// }
+
+// const largestNumber = findLargestNumber(A);
+
+// document.write(`largest Number ${largestNumber}`);
+
+
+const A = [24, 53, 201, 10,500, 78, 91, 12];
+
+const largestNumber = Math.max(...A);
+
+document.write(`largest Number ${largestNumber}`);
