5a23921695d5b042a07f597f95fb836f990519bb
samade747 <samad.e747@gmail.com>
2023-10-07 17:09:46 +0500

    1

---
+++ app13.js
@@ -0,0 +1,12 @@
+// chapter 17-20
+// 1. Declare and initialize an empty multidimensional array.
+// (Array of arrays)
+let emptyArray = [[]];
+
+// 2. Declare and initialize a multidimensional array 
+// representing the following matrix:
+let matrix = [
+    [0, 1, 2, 3],
+    [1, 0, 1, 2],
+    [2, 1, 0, 1]
+]
