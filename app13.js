e255d03ebaffda8c0727f0fb801f2085d02b61a2
samade747 <samad.e747@gmail.com>
2023-10-07 19:00:39 +0500

    222

--- app13.js
+++ app13.js
@@ -1,12 +1,64 @@
-// chapter 17-20
-// 1. Declare and initialize an empty multidimensional array.
-// (Array of arrays)
-let emptyArray = [[]];
+// // chapter 17-20
+// // 1. Declare and initialize an empty multidimensional array.
+// // (Array of arrays)
+// let emptyArray = [[]];
 
-// 2. Declare and initialize a multidimensional array 
-// representing the following matrix:
-let matrix = [
-    [0, 1, 2, 3],
-    [1, 0, 1, 2],
-    [2, 1, 0, 1]
-]
+// // 2. Declare and initialize a multidimensional array 
+// // representing the following matrix:
+// let matrix = [
+//     [0, 1, 2, 3],
+//     [1, 0, 1, 2],
+//     [2, 1, 0, 1]
+// ]
+
+// // 3. Write a program to print numeric counting from 1 to 10.
+// //for ( shuru /;  kahan tak chalega ; increment decremnet )
+// for (let i =1; i <= 10; i++){
+//     console.log(i);
+// }
+
+
+// 4. Write a program to print multiplication table of any 
+// number using for loop. Table number & length should be 
+// taken as an input from user
+
+// let = userinp = +prompt('Enter Value');
+// let = userinp1 = +prompt('Enter Value');
+
+// for(var i=1; i<=userinp1; i++){
+//     document.write(userinp + 'X' + i + '=' + userinp*i, '<br>')
+// }
+
+//5. Write a program to print items of the following array 
+// using for loop:
+// fruits = [“apple”, “banana”, “mango”, “orange”, 
+// “strawberry”]
+
+// let fruits = ['apple', 'banana', 'mango', 'orange', 
+//     'strawberry']
+
+//     for(var j=0; j<=fruits.length; j++){
+//         document.write(fruits[j] + '<br>');
+//     }
+
+
+// 6. Generate the following series in your browser. See 
+// example output.
+// a. Counting: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+// b. Reverse counting: 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
+// c. Even: 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20
+// d. Odd: 1, 3, 5, 7, 9, 11, 13, 15, 17, 19
+// e. Series: 2k, 4k, 6k, 8k, 10k, 12k, 14k, 16k, 18k, 20k
+
+
+
+// for(var i=1; i<=15; i++){
+//   document.write(i + '<br>');
+// }
+
+
+
+for(var j=10; j>=1; j--){
+    document.write(j + '<br>');
+  }
+
