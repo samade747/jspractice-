1fe34c1902646b8e4714faaa5b1f3b7603a9212c
samade747 <samad.e747@gmail.com>
2023-10-08 14:48:03 +0500

    21

---
+++ app14.js
@@ -0,0 +1,51 @@
+// 1. Write a program that takes two user inputs for first and 
+// last name using prompt and merge them in a new variable 
+// titled fullName. Greet the user using his full name.
+
+// function greetUser(){
+//     let firstName = prompt('enter firstName');
+//     let lastName = prompt('enter firstName');
+    
+//     const fullName = (`${firstName} ${lastName}`);
+
+//     document.write(`hello ${fullName}`)
+// }
+
+
+// greetUser()
+
+
+// 2. Write a program to take a user input about his favorite 
+// mobile phone model. Find and display the length of user 
+// input in your browser
+
+// function favoriteMobile(){
+//     let mobileModel = prompt('enter your mobile phone model');
+//     const lengthOfInput = mobileModel.length;
+
+//     document.write(`My favorite phone is : ${mobileModel} <br> The length of your input is ${lengthOfInput}`)
+
+
+// }
+
+// favoriteMobile()
+
+// 3. Write a program to find the index of letter “n” in the word
+// “Pakistani” and display the result in your browser 
+
+// function findIndex(){
+//     const word = prompt('Please Enter a word');
+//     const index = prompt('find index number of keyword you type above');
+
+//     const indexUser = word.indexOf(index);
+
+//     document.write(`the index of ${index} in the ${word} is on ${indexUser}`); 
+// }
+
+// const result = findIndex();
+
+// 4. Write a program to find the last index of letter “l” in the 
+// word “Hello World” and display the result in your browser
+
+
+
