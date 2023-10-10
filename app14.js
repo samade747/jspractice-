3a163fa747cf280f0141ce06507439c9c6f799e9
samade747 <samad.e747@gmail.com>
2023-10-10 11:18:06 +0500

    1121

--- app14.js
+++ app14.js
@@ -199,9 +199,22 @@
 // Remove the dot to display “3536” display in your browser
 
 
-var num = prompt('enter number with . value');
-var numAsString = num.toString();
-var numWithoutDot = numAsString.replace('.' , '');
+// var num = prompt('enter number with . value');
+// var numAsString = num.toString();
+// var numWithoutDot = numAsString.replace('.' , '');
 
-document.write(`Original number ${num} <br>`);
-document.write(`Number without dot ${numWithoutDot} <br>`);
+// document.write(`Original number ${num} <br>`);
+// document.write(`Number without dot ${numWithoutDot} <br>`);
+
+
+// 13. Write a program to take user input and store username 
+// in a variable. If the username contains any special symbol 
+// among [@ . , !], prompt the user to enter a valid username. 
+// For character codes of [@ 
+
+//     Note:
+// ASCII code of ! is 33
+// ASCII code of , is 44
+// ASCII code of . is 46
+// ASCII code of @ is 64
+
