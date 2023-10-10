d38f1b4bbc80c218fcb9f64db631944e5ba3b519
samade747 <samad.e747@gmail.com>
2023-10-10 22:04:01 +0500

    12

--- app15.js
+++ app15.js
@@ -1,3 +1,5 @@
+// chapter 26-30
+
 // 1. Write a program that takes a positive integer from user &
 // display the following in your browser.
 // a. number
@@ -6,4 +8,51 @@
 // d. ceil value of the number
 
 
-let userInput = prompt('Enter Postive number');
+// let userInput = prompt('Enter Postive floating point number');
+// let number = parseFloat(userInput);
+
+// if(number > 0){
+//     let roundOffValue = Math.round(number);
+//     let floorValue = Math.floor(number);
+//     let ceilValue = Math.ceil(number);
+
+//     document.write(`a. Original Number ${number} <br>`);
+//     document.write(`b. Round off Value:  ${roundOffValue} <br>`);
+//     document.write(`b. floor Value:  ${floorValue} <br>`);
+//     document.write(`b. Ceil Value:  ${ceilValue} <br>`);
+// } else {
+//     alert(`pls enter a postive number.`);
+// }
+
+// 2. Write a program that takes a negative floating point
+// number from user & display the following in your browser.
+// a. number
+// b. round off value of the number
+// c. floor value of the number
+// d. ceil value of the number
+
+
+
+// let userInput = prompt('Enter Negative floating point number');
+// let number = parseFloat(userInput);
+
+// if(number < 0){
+//     let roundOffValue = Math.round(number);
+//     let floorValue = Math.floor(number);
+//     let ceilValue = Math.ceil(number);
+
+//     document.write(`a. Original Number ${number} <br>`);
+//     document.write(`b. Round off Value:  ${roundOffValue} <br>`);
+//     document.write(`b. floor Value:  ${floorValue} <br>`);
+//     document.write(`b. Ceil Value:  ${ceilValue} <br>`);
+// } else {
+//     alert(`pls enter a negative floating number.`);
+// }
+
+
+// 3. Write a program that displays the absolute value of a 
+// number.
+// E.g. absolute value of -4 is 4 & absolute value of 5 is 5
+
+var userInput = prompt('enter a number');
+
