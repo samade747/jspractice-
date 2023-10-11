57b457250b2ada3b08c720021db9cdecd11b007a
samade747 <samad.e747@gmail.com>
2023-10-11 14:58:50 +0500

    21325

--- app16.js
+++ app16.js
@@ -9,21 +9,70 @@
 // For example December
 
 
-let months = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December'
-];
+// let months = [
+//     'January',
+//     'February',
+//     'March',
+//     'April',
+//     'May',
+//     'June',
+//     'August',
+//     'September',
+//     'October',
+//     'November',
+//     'December'
+// ];
 
-let currentDate = new Date();
-let currentMonth = months[currentDate.getMonth()];
+// let currentDate = new Date();
+// let currentMonth = months[currentDate.getMonth()];
 
-document.write(`current month is ${currentMonth}`);
+// document.write(`current month is ${currentMonth}`);
+
+
+// 3. Write a program that alerts the first 3 letters of the current
+// day, for example if today is Sunday then alert will show 
+// Sun
+
+
+// let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+// let currentDate = new Date();
+// let currentDay = days[currentDate.getDay()];
+
+// document.write(`current day ${currentDay}`);
+
+
+// 4. Write a program that displays a message “It’s Fun day” if 
+// its Saturday or Sunday today
+
+// let currentDate = new Date();
+// let currentDay = currentDate.getDay();
+
+// if(currentDay === 0 || currentDay === 6) {
+//     document.write(`Its a fun day`);
+// } else {
+//     document.write(`Its not fun day`);
+// }
+
+// 5. Write a program that shows the message “First fifteen 
+// days of the month” if the date is less than 16th of the month 
+// else shows “Last days of the month”.
+
+// let currentDate = new Date ();
+// let currentDay = currentDate.getDate();
+
+// if(currentDay < 16){
+//     document.write(`first 1st fifteen days of month`);
+// } else{
+//     document.write(`last fifteen days of month`);
+// }
+
+
+// 6. Write a program that determines the minutes since 
+// midnight, Jan. 1, 1970 and assigns it to a variable that 
+// hasn't been declared beforehand. Use any variable you like 
+// to represent the Date object
+
+
+
+
