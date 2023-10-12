2f19cc9776a04dabbe0b0a50ad5bfa76bd3c1f2e
samade747 <samad.e747@gmail.com>
2023-10-12 19:49:07 +0500

    aasd

--- app16.js
+++ app16.js
@@ -188,3 +188,33 @@
 // let birthYear = currentYear - age;
 
 // document.write(`Your Birth year is ${birthYear}`);
+
+// 14. Write a program to generate your K-Electric bill in your
+// browser. All the amounts should be rounded off to 2 
+// decimal places. Display the following fields:
+// a. Customer Name
+// b. Current Month
+// c. Number of units
+// d. Charges per unit
+// e. Net Amount Payable (within Due Date)
+// f. Late Payment Surcharge
+// g. Gross Amount Payable (after Due Date
+
+// Net Amount Payable (within Due Date) = Number of units * Charges per unit
+// & Gross Amount Payable (after Due Date) = Net Amount + Late Payment Surcharge
+
+
+function calculateBill(){
+    let numUnits = parseFloat(document.getElementById(`numUnits`).value);
+    let ChargesPerUnit = parseFloat(document.getElementById(`chargesPerUnit`).value);
+
+    let netAmount = numUnits * ChargesPerUnit;
+    let latePaymentSurcharge = netAmount * 0.05;
+
+    let grossAmount = netAmount + latePaymentSurcharge;
+
+    document.getElementById('netAmount').textContent = netAmount.toFixed(2);
+    document.getElementById('latePaymentSurcharge').textContent = latePaymentSurcharge.toFixed(2);
+    document.getElementById('grossAmount').textContent = grossAmount.toFixed(2);
+}
+
