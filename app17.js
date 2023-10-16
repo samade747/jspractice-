cae145a43e381207d75952709de473440e765f4f
samade747 <samad.e747@gmail.com>
2023-10-17 01:58:06 +0500

    2202

---
+++ app17.js
@@ -0,0 +1,12 @@
+// Chapter 35-38 
+
+// 1. Write a function that displays current date & time in your 
+// browser
+
+
+// function currentdate() {
+//     date = new Date() 
+//     return date 
+// }
+
+// document.write(currentdate());
