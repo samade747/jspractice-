46a3f3a05dd2e72cb718d47d48007906d5ca1dc2
samade747 <samad.e747@gmail.com>
2023-10-30 20:22:06 +0500

    5465654

--- appforloop.js
+++ appforloop.js
@@ -770,4 +770,54 @@
 // }
 
 
-var a = (5 == 5 ? 'han Ok' : 'nikal')
+// var a = (5 == 5 ? 'han Ok' : 'nikal')
+
+// Api 
+// json format, javascript object notation 
+// loader chalne ke badh data agye toh reslove chale ga after loading
+// loader chalne ke badh data na aye toh rejet chale ga
+
+// callback function
+// function ke andar parameter ki surat mai 1 aur function pass hojata hai 
+
+// arrow function  
+//                   (parameter)// (reslove or reject) (res, rej)
+let pro = new Promise((resolve, reject) => {
+    let kilwao = 'zinger kilaooo'
+    if(kilwao == 'zinger kilaooo'){
+        resolve('han bahi theek hai')
+    } else {
+        reject('nikal bhai')
+    }
+})
+// promise ka respone ata hai 2 surato mai
+// .done if fulfiled chalte hain
+// .catch if rejected chalte hain
+
+
+// pro ka data //. then is a call back function //(parameter)
+pro.then((data) => {
+    console.log(data)
+// catch is a call back function
+// is promise not fullfiled error show
+}).catch((err) => {
+    console.log(err)
+})
+
+
+
+
+// new promise reslove reject 
+
+let pro1 = new Promise((reslove, reject) =>{
+    let worldCup = 'Pakistan'
+    if(worldCup == 'Pakistan'){
+        reslove('jeet gaya')
+    } else {
+        reject('haar gaya')
+    }
+})
+
+// then is a call back function
+pro1.then(data => console.log(data) )
+.catch(error => console.log(error))
